fbc94d0f9dea1da03fdb371766eb62e74470bff9
Aahil <irjashaik@gmail.com>
2024-04-19 18:02:37 +0200

    2 spieler und schrift Herzen
    Ich habe schriftliche Herzen mit eingeführt . Und 2 Spieler.

--- spaceship.py
+++ spaceship.py
@@ -4,9 +4,15 @@ import random
 WIDTH = 800
 HEIGHT = 602
 
+heart = 3
+
 ship = Actor("player/spaceships/playership1_blue")
 ship.x = 370
 ship.y = 550
+
+ship2 = Actor("player/spaceships/playership1_red")
+ship2.x = 370
+ship2.y = 550
 
 gem = Actor("items/gemgreen")
 gem.x = random.randint(20, 780)
@@ -21,21 +27,32 @@ def on_mouse_move(pos, rel, buttons):
 
 
 def update():
-    global score, game_over
+    global score, game_over, heart
 
     if keyboard.left:
         ship.x = ship.x - 5
     if keyboard.right:
         ship.x = ship.x + 5
 
+    if keyboard.a:
+        ship2.x = ship.x - 5
+    if keyboard.d:
+        ship2.x = ship.x + 5
+
     gem.y = gem.y + 4 + score / 5
-    if gem.y > 600:
-        game_over = True
+
+
+    if heart == 0:
+      game_over = True
+
     if gem.colliderect(ship):
         gem.x = random.randint(20, 780)
         gem.y = 0
         score = score + 1
 
+    if gem.y > 600:
+       heart = heart -1
+       gem.y = 0
 
 def draw():
     screen.fill((80, 0, 70))
@@ -47,8 +64,9 @@ def draw():
     else:
         gem.draw()
         ship.draw()
+        ship2.draw()
         screen.draw.text(
-            "Score: " + str(score), (15, 10), color=(255, 255, 255), fontsize=30
+            "Score: " + str(score) +"       Hearts" + str(heart), (15, 10), color=(255, 255, 255), fontsize=30
         )
 
 
